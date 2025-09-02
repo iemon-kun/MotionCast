97b9fec33daa097349f26a92b9aeacaf8c0f3b82
iemon_kun <13feelingood@gmail.com>
2025-09-02 11:44:54 +0900

    build(app): ESLint/Prettier を導入しスクリプト追加 (#1)
    app に Prettier と eslint-config-prettier を追加。eslint.config.js に eslint-config-prettier を適用し、format/lint スクリプトを追加して整形と修正を容易化。

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -20,4 +21,6 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
+  // Disable formatting-related rules to avoid conflicts with Prettier
+  eslintConfigPrettier,
 ])
